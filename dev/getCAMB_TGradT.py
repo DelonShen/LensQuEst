2129ce9851ec1f1ad32cf2ea34aa04e3001c4596
Delon Shen <delon@stanford.edu>
2023-05-24 15:47:31 -0700

    computed TGradT on laptop because Illegal Instruction on sherlock

--- dev/getCAMB_TGradT.py
+++ dev/getCAMB_TGradT.py
@@ -1,4 +1,5 @@
 import camb
+import pickle
 from camb import model, initialpower
 
 #Set up a new set of parameters for CAMB
@@ -10,14 +11,18 @@ print('computing results')
 results = camb.get_results(pars)
 
 #get dictionary of CAMB power spectra
-print('getting results')
-powers =results.get_cmb_power_spectra(pars, CMB_unit='muK')
-cl = results.get_lens_potential_cls()
-c_lensed = results.get_lensed_scalar_cls()
-c_lens_response = results.get_lensed_gradient_cls()
+print('Getiting cmb power dict')
+powers = results.get_cmb_power_spectra(pars, CMB_unit='muK')
+print('getting PP PT PE')
+cl = results.get_lens_potential_cls( CMB_unit='muK')  # PT PP PE
+print('getting TT EE BB TE')
+c_lensed = results.get_lensed_scalar_cls( CMB_unit='muK') #TT EE BB TE
+print('getting TGradT')
+c_lens_response = results.get_lensed_gradient_cls( CMB_unit='muK') #T GradT and others
 
 print('saving results')
-oup_fname = '/oak/stanford/orgs/kipac/users/delon/LensQuEst/CAMB_outputs.pkl'
+
+oup_fname = '../data/input/universe_Planck15/camb/CAMB_outputs.pkl'
 print(oup_fname)
 f = open(oup_fname, 'wb') 
 pickle.dump((powers,cl,c_lensed,c_lens_response) , f)
