1e1d181f8fa56908607708b6464d32680e4f1322
Delon Liu Shen <delonshen@gmail.com>
2023-08-24 20:21:00 -0700

    even MORE stats for MC N1 correction. I wonder how offended people would be by hybrid N1 correction

--- dev/gen_MCMC_N1_part2_combineTs_runner.py
+++ dev/gen_MCMC_N1_part2_combineTs_runner.py
@@ -1,6 +1,8 @@
 import subprocess
 from tqdm import trange
-for i in trange(10):
-    for j in trange(10):
+for i in trange(20):
+    for j in trange(20):
+        if(i < 10 and j < 10):
+            continue
         line = 'python -u gen_MCMC_N1_part2_combineTs.py %d %d &> logs/2023-08-23-MCN1-part2-%d-%d'%(i, j, i, j)
         result = subprocess.run(line, shell=True, check=True)
